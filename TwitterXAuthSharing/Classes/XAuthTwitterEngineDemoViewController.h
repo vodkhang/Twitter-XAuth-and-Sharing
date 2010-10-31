9f4e047ec62cb1f4dde39fc84d1fe888c35f9a76
Khang Vo <vodkhang@gmail.com>
2010-10-31 13:37:03 +0700

    Remove the Key

--- TwitterXAuthSharing/Classes/XAuthTwitterEngineDemoViewController.h
+++ TwitterXAuthSharing/Classes/XAuthTwitterEngineDemoViewController.h
@@ -10,8 +10,8 @@
 #import "XAuthTwitterEngineDelegate.h"
 #import "OAToken.h"
 
-#define kOAuthConsumerKey		@"INDTRdiJRvIqFplFRECBvw"		// Replace these with your consumer key 
-#define	kOAuthConsumerSecret	@"onQrxHZ1xLWfK7844HEiE9eyxG4B6oUaeXXbOsVs"		// and consumer secret from 
+#define kOAuthConsumerKey		@""		// Replace these with your consumer key 
+#define	kOAuthConsumerSecret	@""		// and consumer secret from 
 										// http://twitter.com/oauth_clients/details/<your app id>
 
 #define kCachedXAuthAccessTokenStringKey	@"cachedXAuthAccessTokenKey"
@@ -19,14 +19,11 @@
 @class XAuthTwitterEngine;
 
 @interface XAuthTwitterEngineDemoViewController : UIViewController <XAuthTwitterEngineDelegate> {
-//	IBOutlet UITextField *usernameTextField;
-//	IBOutlet UITextField *passwordTextField;
 	IBOutlet UIButton *sendTweetButton;
 	
 	XAuthTwitterEngine *twitterEngine;
 }
 
-//@property (nonatomic, retain) UITextField *usernameTextField, *passwordTextField;
 @property (nonatomic, retain) UIButton *sendTweetButton;
 @property (nonatomic, retain) XAuthTwitterEngine *twitterEngine;
 
